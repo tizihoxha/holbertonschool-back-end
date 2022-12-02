6fd78970ddcb7ee72b5cb076174ad07d21b0e761
tizihoxha <tizihoxha@gmail.com>
2022-12-02 13:21:53 -0800

    added

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""extend your Python script to export data in the CSV format"""
+
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    user_name = resp.json().get('username')
+
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+
+    with open(argv[1] + '.csv', 'w') as file:
+        for todo_item in resp.json():
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            writer.writerow([todo_item['userId'],
+                             user_name,
+                             todo_item['completed'],
+                             todo_item['title']])i
