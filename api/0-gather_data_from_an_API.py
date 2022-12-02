ea9e8c1e1a58e0677b51ee9d3647b0a47ea7ad64
tizihoxha <tizihoxha@gmail.com>
2022-12-02 12:42:57 -0800

    added

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress"""
+
+import requests
+from sys import argv
+
+response_API = 'https://jsonplaceholder.typicode.com/'
+if __name__ == "__main__":
+    users = requests.get("{}users/{}".format(response_API, argv[1])).json()
+    todos = requests.get("{}todos?userId={}".format(
+        response_API, argv[1])).json()
+
+    task_completed = [task['title'] for task in todos if task['completed']]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(users["name"], len(task_completed), len(todos)))
+    print("\n".join("\t {}".format(done) for done in task_completed))
