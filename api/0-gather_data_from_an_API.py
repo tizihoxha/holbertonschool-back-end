33334e5911d33c553b53ec8d69b0c44951b31c62
tizihoxha <tizihoxha@gmail.com>
2022-12-02 12:44:37 -0800

    Added

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,6 +2,7 @@
 """Python script that, using this REST API, for a given employee ID, returns
 information about his/her TODO list progress"""
 
+#import json
 import requests
 from sys import argv
 
